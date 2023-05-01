00151e442d04772ae39cb508c77aedc097dc1c69
abaremyi <70200753+abaremyi@users.noreply.github.com>
2023-05-01 12:30:16 -0400

    ch(cors): uncomment import python_heroku

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -154,7 +154,7 @@ JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
